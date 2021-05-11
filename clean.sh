48d54f5f1dda2c97f8be1fe1a5ddc947b8f330e5
Bloomberg2000 <pxfwxhpb@live.com>
2021-05-11 17:36:59 +0800

    Add make.sh & clean.sh

---
+++ clean.sh
@@ -0,0 +1,14 @@
+rm missfont.log
+rm *.aux
+rm *.bbl
+rm *.blg
+rm *.log
+rm *.out
+# rm *.pdf
+rm *.synctex.gz
+rm *.toc
+rm *.fls
+rm *.fdb_latexmk
+rm -rf _minted*
+rm *.xdv
+rm *.synctex\(busy\)
